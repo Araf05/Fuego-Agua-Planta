1b801bc8ef5276635106b41f84a9621db9c2d9f1
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2024-03-30 02:11:22 -0300

    Ciclo del juego, en progreso

--- app.js
+++ app.js
@@ -4,20 +4,31 @@ function aleatorio(min, max){
     return Math.floor( Math.random( max - min + 1 ) + min );
 }
 
+
 let jugador = 0;
-let pc = aleatorio(1, 3);
+let pc = 0;
+let ganados = 0;
+let perdidos = 0;
 
-jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijeras");
+while(ganados < 3 && perdidos < 3){
+    pc= aleatorio(1, 3);
+    jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijeras");
 
-alert("Elegiste " + opciones[jugador-1]);
-alert("Pc eligio " + opciones[pc-1]);
+    alert("Elegiste " + opciones[jugador-1]);
+    alert("Pc eligio " + opciones[pc-1]);
 
-if(pc == jugador){
-    alert("EMPATE");
-} else if(jugador == 1 && pc == 3 || jugador == 2 && pc == 1 || jugador == 3 && pc == 2){
-    alert("GANASTE");
-} else if(jugador == 1 && pc == 2 || jugador == 2 && pc == 3 || jugador == 3 && pc == 1){
-    alert("PERDISTE");
-} else {
-    alert("Opción no valida!");
+    if(pc == jugador){
+        alert("EMPATE");
+    } else if(jugador == 1 && pc == 3 || jugador == 2 && pc == 1 || jugador == 3 && pc == 2){
+        alert("GANASTE");
+        ganados += 1;
+    } else if(jugador == 1 && pc == 2 || jugador == 2 && pc == 3 || jugador == 3 && pc == 1){
+        alert("PERDISTE");
+        perdidos += 1;
+    } else {
+        alert("Opción no valida!");
+    }
 }
+
+alert("Ganaste " + ganados + " veces. Perdiste " + perdidos + " veces.");
+
